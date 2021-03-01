c1d45abd6cb333b96641a569fc7560e147cb58da
Juan Manuel <jm.manuelvg@gmail.com>
2021-02-28 19:35:43 -0500

    Added loggin API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -2,6 +2,10 @@
 
 from profiles_api import views
 from django.urls import path, include
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
 
 from rest_framework.routers import DefaultRouter
 
@@ -12,5 +16,6 @@ router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
